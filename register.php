2896a58cf31868ba9dae6e1b39b49bfc09274524
abhi241092 <abhi241092@gmail.com>
2018-05-12 21:06:44 +0530

    add sql

--- register.php
+++ register.php
@@ -1,3 +1,4 @@
+<?php include ("/include/connection.php"); ?>
 <?php include("/include/header.php"); ?>
 <?php include("/include/navigation.php"); ?>
 <style>
@@ -45,15 +46,15 @@
             <table>
                 <tr>
                     <td style="width:50%; padding : 10px;">Name:</td>
-                    <td  colspan="2"><input type="text" id="name" /></td>
+                    <td  colspan="2"><input type="text" name="name" id="name" /></td>
                 </tr>
                 <tr>
                     <td style="width:50%; padding : 10px;">Email:</td>
-                    <td colspan="2"><input type="text" /></td>
+                    <td colspan="2"><input type="text" name="email" /></td>
                 </tr>
                 <tr>
                     <td  style="width:50%; padding : 10px;">Mobile:</td>
-                    <td><input type="text" id="mobile"/></td>
+                    <td><input type="text" name="mobile" id="mobile"/></td>
 
                     <td><input type="button" name="Verify" id="verify_otp" value="Verify" onclick="document.getElementById('votp').style.display = 'block';" placeholder="Enter OTP" maxlength="6">
                     <td>
@@ -65,10 +66,33 @@
 
                 </tr>
                 <tr>
-                    <td colspan="3" style="text-align:center;"><input type="button" value="Submit" id="submit"/></td>
+                    <td colspan="3" style="text-align:center;"><input type="submit" value="submit" name="submit" id="submit"/></td>
                 </tr>
             </table>
         </form>
+        <?php
+
+if($_POST['submit'])
+{
+       $nm = $_POST['name'];
+       $mn = $_POST['mobile'];
+       $eml = $_POST['email'];
+
+
+if($nm!="" && $mn!="" && $eml!="")
+   {
+	$query= "INSERT INTO REGISTER VALUES('$nm','$mn','$eml')";
+    $data = mysqli_query($conn, $query);
+
+	if($data){echo "Data saved into database successfully";}
+   }
+   else{
+	        echo "All fields are required";
+        }
+
+}
+
+?>
     </div>
     <div style="clear:both;"></div>
 </div>
---
+++ sql/register.sql
@@ -0,0 +1,56 @@
+-- phpMyAdmin SQL Dump
+-- version 4.2.7.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 12, 2018 at 05:32 PM
+-- Server version: 5.6.20
+-- PHP Version: 5.5.15
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `insert`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `register`
+--
+
+CREATE TABLE IF NOT EXISTS `register` (
+  `username` varchar(20) NOT NULL,
+  `mobileno` int(50) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `password` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `register`
+--
+
+INSERT INTO `register` (`username`, `mobileno`, `email`, `password`) VALUES
+('abhi241092', 2147483647, 'abhi241092@gmail.com', 'abhi'),
+('abhi241094', 2147483647, 'qw@gmail.com', 'qwerty');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `register`
+--
+ALTER TABLE `register`
+ ADD PRIMARY KEY (`username`,`mobileno`,`email`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
